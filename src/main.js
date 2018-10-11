549ea576a8ba83209abeda80d529a444453664a3
Chan Lee and Regina Nurieva <chan@>
2018-10-11 09:44:57 -0700

    finish the project

--- src/main.js
+++ src/main.js
@@ -2,18 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { Haiku } from './haiku.js';
+import { Haiku } from './haiku.js';
 
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault();
-    // let first = $("#first").val();
-    // let second = $("#second").val();
-    // let third = $("#third").val();
-    // let weekdayInstance = new Haiku(first,second,third);
-    // let weekdayInt = weekdayInstance.getDay();
+    let first = $("#first").val();
+    let second = $("#second").val();
+    let third = $("#third").val();
+    let haikuInstance = new Haiku(first,second,third);
+    let haikuResult = haikuInstance.haikuChecker();
 
-    // let result = dayOfWeekAsString(weekdayInt);
-    // $("#result").text(`${result}`);
+    $("#result").text(`${haikuResult}`);
   });
 });
